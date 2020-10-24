6edbc4eb1fd5ae8d7d18b26c37aff7fc3d4a1870
treb0ry <vinodel2589@gmai.com>
2020-10-24 22:53:12 +0300

    added AddItemButton style

--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+interface AddItemButtonProps {
+  dark?: boolean;
+}
 
 export const AppContainer = styled.div`
   align-items: flex-start;
@@ -33,4 +36,20 @@ export const CardContainer = styled.div`
   max-width: 300px;
   border-radius: 3px;
   box-shadow: #091e4240 0px 1px 0px 0px;
+`;
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+  background-color: #ffffff3d;
+  border-radius: 3px;
+  border: none;
+  color: ${(props) => (props.dark ? "#000" : "fff")};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &hover {
+    background-color: #ffffff52;
+  }
 `;
