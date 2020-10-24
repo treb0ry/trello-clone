f10f260e4f7ae53882b67a653e9d6af8764365fc
treb0ry <vinodel2589@gmai.com>
2020-10-24 22:45:47 +0300

    added plaseholder to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Column } from "./Column";
+import { Card } from "./Card";
 import { AppContainer } from "./styles";
 function App() {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold"></Card>
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TypeScript"></Card>
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing"></Card>
+      </Column>
+    </AppContainer>
+  );
 }
 
 export default App;
