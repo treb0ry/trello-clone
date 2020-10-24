86a6a6702380c26eb07f203989c9388c4e19c8ef
treb0ry <vinodel2589@gmai.com>
2020-10-24 22:59:06 +0300

    added NewItemInput  style

--- src/styles.ts
+++ src/styles.ts
@@ -71,3 +71,12 @@ export const NewItemButton = styled.button`
   padding: 6px 12px;
   text-align: center;
 `;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+`;
